79638d2ecf233fd3b7b9aed91e9652cb01a92b1c
MPia2 <pichu135@gmail.com>
2021-04-05 20:06:45 -0300

    arreglo Port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path= require ('path');
-const port = process.env.Port
+const port = process.env.PORT;
 app.use(express.static('public'))
 
 
